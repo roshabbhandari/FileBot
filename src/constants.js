770b814c2e5df8f8ad86779dddee37402ada7b9d
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-08-27 20:25:29 +0545

    feat: define ignorable system files

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const CATEGORIES = Object.freeze({
-  Images: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.bmp'],
+  Images: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.bmp', '.ico'],
   Videos: ['.mp4', '.mkv', '.mov', '.avi', '.webm'],
   Audio: ['.mp3', '.wav', '.flac', '.aac', '.ogg'],
   Documents: ['.pdf', '.doc', '.docx', '.txt', '.rtf'],
@@ -9,5 +9,6 @@ const CATEGORIES = Object.freeze({
 });
 
 const DEFAULT_CATEGORY = 'Others';
+const IGNORE_NAMES = new Set(['desktop.ini', 'Thumbs.db']);
 
-module.exports = { CATEGORIES, DEFAULT_CATEGORY };
+module.exports = { CATEGORIES, DEFAULT_CATEGORY, IGNORE_NAMES };
