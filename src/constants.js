b97f6d9341d8893e5559e98094e0386fc7e0b1f6
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-08-27 20:25:34 +0545

    feat: support mpeg and m4a files

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const CATEGORIES = Object.freeze({
   Images: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.bmp', '.ico'],
-  Videos: ['.mp4', '.mkv', '.mov', '.avi', '.webm'],
-  Audio: ['.mp3', '.wav', '.flac', '.aac', '.ogg'],
+  Videos: ['.mp4', '.mkv', '.mov', '.avi', '.webm', '.mpeg'],
+  Audio: ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.m4a'],
   Documents: ['.pdf', '.doc', '.docx', '.txt', '.rtf'],
   Spreadsheets: ['.xls', '.xlsx', '.csv'],
   Archives: ['.zip', '.rar', '.7z', '.tar', '.gz'],
