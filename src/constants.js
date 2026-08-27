facacd86f014d8d507e4341a0df737f0f363f1ce
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-08-27 20:24:42 +0545

    feat: add file category definitions

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CATEGORIES = Object.freeze({
+  Images: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg'],
+  Videos: ['.mp4', '.mkv', '.mov', '.avi', '.webm'],
+  Audio: ['.mp3', '.wav', '.flac', '.aac', '.ogg'],
+  Documents: ['.pdf', '.doc', '.docx', '.txt', '.rtf'],
+  Spreadsheets: ['.xls', '.xlsx', '.csv'],
+  Archives: ['.zip', '.rar', '.7z', '.tar', '.gz'],
+  Programs: ['.exe', '.msi', '.bat', '.cmd']
+});
+
+const DEFAULT_CATEGORY = 'Others';
+
+module.exports = { CATEGORIES, DEFAULT_CATEGORY };
